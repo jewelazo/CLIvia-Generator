eb3bdf0d3375d069f6f4eca32e533b9c88d5e3a9
jewelazo <gpalominoe@uni.pe>
2021-07-17 04:53:00 +0000

    refactoring code

--- trivia_generator.rb
+++ trivia_generator.rb
@@ -1,6 +1,3 @@
-# do not forget to require your gem dependencies
-# do not forget to require_relative your local dependencies
-# rubocop:disable Style/GuardClause,Metrics/AbcSize,Metrics/MethodLength
 require_relative "./presenter"
 require_relative "./requester"
 require_relative "./services/opentdb"
@@ -8,20 +5,13 @@ require "json"
 require "terminal-table"
 require "htmlentities"
 
-file_name = ARGV.shift
-file_name = "scores.json" if file_name.nil?
-
 class TriviaGenerator
   include Presenter
   include Requester
-  # maybe we need to include a couple of modules?
-
-  def initialize(filename)
+  def initialize(filename = "scores.json")
     @score = 0
-    # @score_data = []
     @filename = filename
     @score_data = JSON.parse(File.read(@filename), symbolize_names: true)
-    # we need to initialize a couple of properties here
   end
 
   def start
@@ -35,32 +25,19 @@ class TriviaGenerator
       print_welcome
       action = select_main_menu_action
     end
-    # File.open("scores.json", "w") do |json|
-    #   json.write(@score_data.to_json)
-    # end
     print_exit
   end
 
   def random_trivia
     questions = parse_questions[:results]
+    ask_questions(questions)
+    save
+    @score = 0
+  end
+
+  def ask_questions(questions)
     questions.each do |q|
-      puts "Category: #{q[:category]} | Difficulty: #{q[:difficulty]}"
-      puts "Question: #{HTMLEntities.new.decode q[:question]}"
-      array = []
-      array[0] = q[:correct_answer]
-      options = array.concat(q[:incorrect_answers])
-      options = options.map { |str| HTMLEntities.new.decode(str) }
-      n = (1..options.size)
-      options_u = options.sort_by { rand }
-      dic = n.zip(options_u).to_h
-      dic.each { |k, v| puts "#{k}. #{v}" }
-      print "> "
-      input = gets.chomp.to_i
-      until (1..options_u.size).include?(input)
-        puts "invalid option"
-        print "> "
-        input = gets.chomp.to_i
-      end
+      input, dic = ask_question(q)
       if dic[input].match?(/#{q[:correct_answer]}/)
         @score += 10
         puts "Correct answer!!!"
@@ -69,54 +46,20 @@ class TriviaGenerator
         puts "The correct answer was: #{q[:correct_answer]}"
       end
     end
-    puts "Well done! Your score is #{@score}"
-    save(@score)
-    @score = 0
   end
 
-  def ask_questions
-    # ask each question
-    # if response is correct, put a correct message and increase score
-    # if response is incorrect, put an incorrect message, and which was the correct answer
-    # once the questions end, show user's score and promp to save it
-  end
-
-  def save(_data)
+  def save
     puts "--------------------------------------------------"
-    print "Do you want to save your score? y/n "
-    save = gets.chomp.downcase
-    alt = %w[y n]
-    until alt.include?(save)
-      puts "Invalid option"
-      print "Do you want to save your score? y/n "
-      save = gets.chomp.downcase
-    end
-    if save != "n"
-      puts "Type the name to assign to the score"
-      print "> "
-      name = gets.chomp
-      name = "Anonymous" unless name != ""
-      @score_data << { name: name, score: @score }
-      File.open("scores.json", "w") do |json|
-        json.write(@score_data.to_json)
-      end
-    end
-  end
-  # write to file the scores data
-
-  def parse_scores
-    # get the scores data from file
+    will_save?
   end
 
   def load_questions
     Opentdb.index
-    # ask the api for a random set of questions
   end
 
   def parse_questions
     load_response = load_questions
     JSON.parse(load_response.body, symbolize_names: true)
-    # questions came with an unexpected structure, clean them to make it usable for our purposes
   end
 
   def print_scores
@@ -129,10 +72,7 @@ class TriviaGenerator
     end
     puts table
   end
-  #   # print the scores sorted from top to bottom
-  # end
 end
 
-trivia = TriviaGenerator.new(file_name)
+trivia = TriviaGenerator.new
 trivia.start
-# rubocop:enable Style/GuardClause,Metrics/AbcSize,Metrics/MethodLength
